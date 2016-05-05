635466a05bcd300fb40b5ce24d0aedc90bfbddbe
yujungchen <steiff.chen@gmail.com>
2016-05-05 17:10:42 +0800

    generate gaussian sample

---
+++ gaussian.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+import sys
+
+#Test 2D radnom number generation
+def RandomSampling():
+	x = rnd.random() - 0.5
+	y = rnd.random() - 0.5
+	return (x, y)
+
+#Test 2D exponential random number generation	
+def ExponentialSampling(_sigma_tr):
+	Seed = rnd.random()
+	DiscR = -1.0 * np.log(1.0 - Seed) / _sigma_tr
+	TempDisc = 0.5 * np.sqrt(rnd.random())
+	rndangle = rnd.random()
+	angle = 2.0 * np.pi * rndangle
+	x = DiscR * np.cos(angle)
+	y = DiscR * np.sin(angle)
+	return (x, y)
+
+#Test 2D gaussian random number generation	
+def GaussianSampling(mu_x, sigma_x, mu_y, sigma_y, rho):
+	u1 = rnd.random()
+	u2 = rnd.random()
+	r = np.sqrt(-2.0 * np.log(u1))
+	theta = 2.0 * np.pi * u2
+	nx = r * np.cos(theta)
+	ny = r * np.sin(theta)
+	x = sigma_x * nx + mu_x
+	y = sigma_y * (rho * nx + np.sqrt(1.0 - rho * rho) * ny) + mu_y
+	return (x, y)
+
+
+array1 = [ ]
+array2 = [ ]
+array3 = [ ]
+array4 = [ ]
+
+
+total = len(sys.argv)
+cmdargs = str(sys.argv)
+
+# NUmber of samples
+sampleNum = int(sys.argv[1])
+
+
+for i in range(sampleNum):
+	#Uniform Random sampling
+	#x, y = RandomSampling()
+	rx, ry = GaussianSampling(0.0, 1.0, 0.0, 1.0, 0.75)
+	array1.append(rx)
+	array2.append(ry)
+
+
+mean_array1 = np.mean(array1)
+mean_array2 = np.mean(array2)
+print ('Sample Mean (', "%.5f" %mean_array1, "%.5f" %mean_array2, ')')
+
+#for i in range(sampleNum):
+#	print (array1[i], array2[i])
+
+#Figure configuration
+plt.figure(figsize = (10, 10))
+plt.plot(array1, array2, 'r.')
+plt.plot(array3, array4, 'g.')
+plt.title('Distribution Plot')
+plt.grid(True)
+plt.axis([-10.0, 10.0, -10.0, 10.0])
+plt.axes().set_aspect('equal', 'datalim')
+plt.show()
